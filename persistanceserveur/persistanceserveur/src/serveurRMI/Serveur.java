8af217412c97cc7fb79e7eb825ef43beed0b82fa
CyrielleM <min.cyrielle@yahoo.com>
2016-03-29 18:00:27 +0200

    Serveur

--- persistanceserveur/persistanceserveur/src/serveurRMI/Serveur.java
+++ persistanceserveur/persistanceserveur/src/serveurRMI/Serveur.java
@@ -1,35 +1,27 @@
 package serveurRMI;
 
 import java.rmi.Naming;
-import java.rmi.RMISecurityManager;
 import java.rmi.Remote;
-import java.rmi.registry.LocateRegistry;
-import java.rmi.registry.Registry;
 import java.rmi.server.RMIClassLoader;
-import java.util.Properties;
-
-import serviceRMIImplementation.ServiceJoueurDTO;
-
 public class Serveur {
-
-	@SuppressWarnings("rawtypes")
 	public static void main(String[] args) {
-		System.setSecurityManager(new RMISecurityManager());
+		if (System.getSecurityManager() == null) 
+			System.setSecurityManager(new SecurityManager());
 		try {
-			Properties p= System.getProperties();
-			String url=p.getProperty("java.rmi.server.codebase"); 
-			Class ClasseServeur = RMIClassLoader.loadClass(url,"ServiceJoueurDTO"); 
-			Naming.rebind("rmi://localhost:1099/ServiceJoueurDTO",(Remote)ClasseServeur.newInstance());
+		    //String url = System.getProperty("java.rmi.server.codebase");
+			Class ServiceJoueurDTO = RMIClassLoader.loadClass("file:/C:/Users/Cyrielle/Desktop/bin/","serviceRMIImplementation.ServiceJoueurDTO");
+			System.out.println("Serveur Bonjour "+ ServiceJoueurDTO.getName());
+			Object o = ServiceJoueurDTO.newInstance();
+			Naming.rebind("rmi://10.0.2.15/ServiceJoueurDTO",(Remote) o);
+			System.out.println("Serveur Bonjour");
 			System.out.println("Objet Service dans le RMIregistry");
 			System.out.println("Attente des invocations des clients ...");
 		}catch (Exception e) {
-			System.out.println("Erreur de liaison de l'objet Reverse");
-			System.out.println(e.toString());
+			System.out.println("Erreur de liaison de l'objet ServiceDTO");
+			e.printStackTrace();
 		}
 	} 
 } 
 	
 	
 
-
-}
