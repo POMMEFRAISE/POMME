0d4875470bb1ba5fd9aaa8a676df4b76eb289970
natier <dylan.natier@laposte.net>
2016-03-31 19:26:23 +0200

    security policy test

--- persistanceserveur/persistanceserveur/src/serveurRMI/Serveur.java
+++ persistanceserveur/persistanceserveur/src/serveurRMI/Serveur.java
@@ -9,7 +9,7 @@ import java.util.Properties;
 public class Serveur {
 	public static void main(String[] args) {
 		if (System.getSecurityManager() == null) 
-			System.setProperty("java.security.policy", "file:/C:/Users/Dylan/git/PDS/persistanceserveur/persistanceserveur/bin/serveurRMI/server.policy");
+			//System.setProperty("java.security.policy", "file:/C:/Users/Dylan/git/PDS/persistanceserveur/persistanceserveur/bin/serveurRMI/server.policy");
 			//System.setProperty("java.rmi.server.codebase", "file:/C:/Users/Dylan/git/PDS/persistanceservice/persistanceservice/bin/");
 			System.setSecurityManager(new SecurityManager());
 		try {
@@ -17,8 +17,6 @@ public class Serveur {
 			String url="file:/C:/Users/Dylan/git/PDS/persistanceservice/persistanceservice/bin/";
 			Class<?> serviceJoueurDTO = RMIClassLoader.loadClass(url,"serviceRMIImplementation.ServiceJoueurDTO");
 			registry.rebind("ServiceJoueurDTO",(Remote)serviceJoueurDTO.newInstance());
-			
-			
 			System.out.println("Serveur Bonjour");
 		}catch (Exception e) {
 			System.out.println("Erreur de liaison de l'objet ServiceDTO");
