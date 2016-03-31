c81708e7f8c40c61e444559ffbdc6a3178cfdd5d
natier <dylan.natier@laposte.net>
2016-03-31 19:35:21 +0200

    serveur

--- persistanceserveur/persistanceserveur/src/serveurRMI/Serveur.java
+++ persistanceserveur/persistanceserveur/src/serveurRMI/Serveur.java
@@ -8,7 +8,7 @@ public class Serveur {
 	public static void main(String[] args) {
 		if (System.getSecurityManager() == null) 
 			System.setProperty("java.security.policy", "file:/C:/Users/Dylan/git/PDS/persistanceserveur/persistanceserveur/bin/serveurRMI/server.policy");
-			System.setProperty("java.rmi.server.codebase", "file:/C:/Users/Dylan/git/PDS/persistanceservice/persistanceservice/bin/");
+			//System.setProperty("java.rmi.server.codebase", "file:/C:/Users/Dylan/git/PDS/persistanceservice/persistanceservice/bin/");
 			System.setSecurityManager(new SecurityManager());
 		try {
 			Registry registry = LocateRegistry.createRegistry(1099);
