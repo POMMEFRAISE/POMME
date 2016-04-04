43ef9cbe2e3830f503647cae69731edbad3075e4
natier <dylan.natier@laposte.net>
2016-04-04 17:58:38 +0200

    Client

--- persistanceclient/persistanceclient/src/clientRMI/Client.java
+++ persistanceclient/persistanceclient/src/clientRMI/Client.java
@@ -1,44 +1,32 @@
 package clientRMI;
 
 import java.lang.reflect.Constructor;
+import java.lang.reflect.Method;
 import java.net.MalformedURLException;
+import java.rmi.Naming;
+import java.rmi.Remote;
 import java.rmi.server.RMIClassLoader;
+
 
 
 public class Client {
 	public static void main(String[] argv) throws Exception {
-    	if (System.getSecurityManager() == null) 
-			System.setProperty("java.security.policy", "file:/C:/Users/Cyrielle/git/PDS/persistanceclient/persistanceclient/bin/client.policy");
+		if (System.getSecurityManager() == null) 
+			System.setProperty("java.security.policy", "file:/C:/Users/Dylan/git/PDS/persistanceclient/persistanceclient/bin/clientRMI/client.policy");
 			System.setSecurityManager(new SecurityManager());
 			
-			try {
+				//url
+				String url="file:/C:/Users/Dylan/git/PDS/persistanceservice/persistanceservice/bin/";
+				
 				//Appelle de la classe JoueurDTO
-				String url="file:/C:/Users/Cyrielle/git/PDS/persistanceservice/persistanceservice/bin/";
-				Class<?> joueurDTO = RMIClassLoader.loadClass(url,"dto.JoueurDTO");
-				Object objectJoueurDTO = joueurDTO.newInstance();
-				System.out.println(objectJoueurDTO);
+				Class<?> serviceJoueur = RMIClassLoader.loadClass(url,"serviceFacade.ServiceFacade");
+				Object serviceJoueurDTO = serviceJoueur.newInstance();
+				System.out.println(serviceJoueurDTO.getClass());
+				Method methode = serviceJoueur.getMethod("getServiceJoueurDTO");
+				//System.out.println(serviceJoueurDTO.getClass().getDeclaredMethod("getServiceJoueurDTO").invoke(serviceJoueurDTO));
+				System.out.println(methode.getReturnType());
+				Remote method = (Remote)Naming.lookup("//127.0.0.1/ServiceJoueurDTO");
 				
-				
-				Class<?> serviceRMIInterface = RMIClassLoader.loadClass(url,"serviceClient.ServiceClient");
-				Constructor<?> c = serviceRMIInterface.getConstructor();
-				c.newInstance();
-				
-				
-				
-			} catch (MalformedURLException e) {
-				// TODO Auto-generated catch block
-				e.printStackTrace();
-			} catch (ClassNotFoundException e) {
-				// TODO Auto-generated catch block
-				e.printStackTrace();
-			} catch (InstantiationException e) {
-				// TODO Auto-generated catch block
-				e.printStackTrace();
-			} catch (IllegalAccessException e) {
-				// TODO Auto-generated catch block
-				e.printStackTrace();
-			}
 	}
-			
-	
+		
 }
