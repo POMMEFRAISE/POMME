ea5976620e4cb4ec6bf950bef331e2a329d24def
natier <dylan.natier@laposte.net>
2016-04-05 13:07:54 +0200

    RMI Client

--- persistanceclient/persistanceclient/src/clientRMI/Client.java
+++ persistanceclient/persistanceclient/src/clientRMI/Client.java
@@ -1,7 +1,6 @@
 package clientRMI;
 
 import java.lang.reflect.Constructor;
-import java.lang.reflect.Method;
 import java.net.MalformedURLException;
 import java.rmi.Naming;
 import java.rmi.Remote;
@@ -12,20 +11,18 @@ import java.rmi.server.RMIClassLoader;
 public class Client {
 	public static void main(String[] argv) throws Exception {
 		if (System.getSecurityManager() == null) 
-			System.setProperty("java.security.policy", "file:/C:/Users/Dylan/git/PDS/persistanceclient/persistanceclient/bin/clientRMI/client.policy");
+			System.setProperty("java.security.policy", "file:/C:/Users/Cyrielle/git/PDS/persistanceclient/persistanceclient/bin/clientRMI/client.policy");
 			System.setSecurityManager(new SecurityManager());
 			
 				//url
-				String url="file:/C:/Users/Dylan/git/PDS/persistanceservice/persistanceservice/bin/";
+				String url="file:/C:/Users/Cyrielle/git/PDS/persistanceservice/persistanceservice/bin/";
 				
 				//Appelle de la classe JoueurDTO
 				Class<?> serviceJoueur = RMIClassLoader.loadClass(url,"serviceFacade.ServiceFacade");
 				Object serviceJoueurDTO = serviceJoueur.newInstance();
-				System.out.println(serviceJoueurDTO.getClass());
-				Method methode = serviceJoueur.getMethod("getServiceJoueurDTO");
-				//System.out.println(serviceJoueurDTO.getClass().getDeclaredMethod("getServiceJoueurDTO").invoke(serviceJoueurDTO));
-				System.out.println(methode.getReturnType());
-				Remote method = (Remote)Naming.lookup("//127.0.0.1/ServiceJoueurDTO");
+				serviceJoueurDTO = serviceJoueurDTO.getClass().getDeclaredMethod("getServiceJoueurDTO", null).invoke(serviceJoueurDTO, null);
+				serviceJoueurDTO = Naming.lookup("//127.0.0.1/ServiceJoueurDTO");
+				System.out.println(serviceJoueurDTO);
 				
 	}
 		
