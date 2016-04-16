007086a4e6cc57a75ac40b5b7299c63fd98aa0bb
CyrielleM <min.cyrielle@yahoo.com>
2016-04-16 19:10:46 +0200

    serveur

--- persistanceserveur/persistanceserveur/src/serveurRMI/Serveur.java
+++ persistanceserveur/persistanceserveur/src/serveurRMI/Serveur.java
@@ -7,14 +7,14 @@ import java.rmi.server.RMIClassLoader;
 public class Serveur {
 	public static void main(String[] args) {
 		if (System.getSecurityManager() == null) 
-			System.setProperty("java.security.policy", "file:/C:/Users/Dylan/git/PDS/persistanceserveur/persistanceserveur/bin/serveurRMI/server.policy");
-			//System.setProperty("java.rmi.server.codebase", "file:/C:/Users/Dylan/git/PDS/persistanceservice/persistanceservice/bin/");
+			System.setProperty("java.security.policy", "file:/C:/Users/Cyrielle/git/PDS/persistanceserveur/persistanceserveur/bin/serveurRMI/server.policy");
+			System.setProperty("java.rmi.server.codebase", "file:/C:/Users/Cyrielle/git/PDS/persistanceservice/persistanceservice/bin/");
 			System.setSecurityManager(new SecurityManager());
 		try {
 			Registry registry = LocateRegistry.createRegistry(1099);
-			String url="file:/C:/Users/Dylan/git/PDS/persistanceservice/persistanceservice/bin/";
-			Class<?> serviceJoueurDTO = RMIClassLoader.loadClass(url,"serviceRMIImplementation.ServiceJoueurDTO");
-			registry.rebind("ServiceJoueurDTO",(Remote)serviceJoueurDTO.newInstance());
+			String url="file:/C:/Users/Cyrielle/git/PDS/persistanceservice/persistanceservice/bin/";
+			Class<?> serviceJoueur = RMIClassLoader.loadClass(url,"serviceRMIImplementation.ServiceJoueurImpl");
+			registry.rebind("ServiceJoueur",(Remote)serviceJoueur.newInstance());
 			System.out.println("Serveur Bonjour");
 		}catch (Exception e) {
 			System.out.println("Erreur de liaison de l'objet ServiceDTO");
